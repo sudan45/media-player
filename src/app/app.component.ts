4ad8994e105d95cb29885b36e97774c1b8aa53a2
Sudan <sudan.bhandari@newitventure.com>
2022-03-23 00:12:03 +0545

    seek bar,volume control and play pause stop implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,80 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'media_player';
-  mp3="../assets/test.mp3"
+  mp3 = "../assets/test.mp3"
+  audioEvents = [
+    "ended",
+    "error",
+    "play",
+    "playing",
+    "pause",
+    "timeupdate",
+    "canplay",
+    "loadedmetadata",
+    "loadstart"
+  ];
+  current_time:any="0"
+  duration:any="0"
+  seek:any=0
+  audio = new Audio()
+  streamObserver(mp3: any) {
+    return new Observable(observer => {
+      this.audio.src = this.mp3
+      this.audio.play()
+
+      const handler = (event: Event) => {
+        this.seek=this.audio.currentTime
+        this.duration=this.formatTime(this.audio.duration)
+        this.current_time=this.formatTime(this.audio.currentTime)
+      }
+      // add event call or sene the event
+      this.addevent(this.audio, this.audioEvents, handler)
+
+
+    })
+  }
+  formatTime(time: number, format: string = "HH:mm:ss") {
+    console.log(time)
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
+  // addd event  functions
+  addevent(obj: any, events: any, handler: any) {
+    events.forEach((event: any) => {
+      obj.addEventListener(event, handler)
+    })
+
+  }
+  // remove event functions
+  // removeevent(obj: any, events: any, handler: any) {
+  //   events.forEach((event: any) => {
+  //     obj.removeEventListener(event, handler)
+  //   })
+
+  // }
+
+
+  setVolume(e: any) {
+    this.audio.volume = e.target.value
+  }
+
+  play() {
+    this.streamObserver(this.mp3).subscribe(event => { })
+  }
+
+  pause() {
+    this.audio.pause()
+  }
+
+  stop() {
+    this.audio.pause()
+    this.audio.currentTime = 0
+  }
+  seektoplay(e:any){
+    this.formatTime(this.audio.currentTime=e.target.value)
+
+  }
+
+
 }
+
